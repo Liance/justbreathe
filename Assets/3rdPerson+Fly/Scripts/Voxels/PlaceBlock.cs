11083cc41525c898836cff93cfecb12f57cd0fa5
Sinead Urisohn <sinzara611@gmail.com>
2020-01-31 21:39:57 +0000

    Prototype scene and static block prefab added

---
+++ Assets/3rdPerson+Fly/Scripts/Voxels/PlaceBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBlock : MonoBehaviour
+{
+    [SerializeField]
+    private bool isPlacingBlock;
+    [SerializeField]
+    private GameObject blockPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
