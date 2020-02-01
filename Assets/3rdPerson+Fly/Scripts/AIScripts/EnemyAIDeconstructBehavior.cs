bfbcc9a2743130d24c1c7ac2e338bf4958e851fa
Kristiyan <stoyanov.krs@gmail.com>
2020-02-01 10:23:38 +0000

    AI Wandering Behavior

---
+++ Assets/3rdPerson+Fly/Scripts/AIScripts/EnemyAIDeconstructBehavior.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyAIDeconstructBehavior : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent agent;
+    public Transform target;
+
+    public enum AIState { WANDERING, NAVIGATING, DESTROYING }
+    public GameObject[] wanderWaypoints;
+    public int[] visitedWaypoints;
+    public int currentWaypoint = 0;
+    public AIState state = AIState.WANDERING;
+    private Transform playerTransform;
+    private LayerMask aiLayerMask;
+
+    private Transform playerLastSeenTransform;
+
+    private bool isWandering = false;
+    private bool isNavigating = false;
+    private bool isDestroying = false;
+
+    public float viewingAngle = 60.0f;
+    public float visibilityDist = 60.0f;
+
+    private WaitForSeconds wanderRefreshDuration = new WaitForSeconds(1.0f);
+    private WaitForSeconds navigateRefreshDuration = new WaitForSeconds(1.0f);
+    private WaitForSeconds destroyRefreshDuration = new WaitForSeconds(1.0f);
+
+    private Vector3 dirToPlayer = new Vector3();
+    private Vector3 dirToLastSeenPlayer = new Vector3();
+
+    public float waypointMinReachDist = 5.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        wanderWaypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        visitedWaypoints = new int[wanderWaypoints.Length];
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        aiLayerMask = LayerMask.NameToLayer("Bot");
+        SetNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+            agent.SetDestination(target.position);
+
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            agent.Move(agent.desiredVelocity);
+        }
+        
+        dirToPlayer = playerTransform.position - transform.position;
+        switch (state)
+        {
+            case AIState.WANDERING:
+                if (!isWandering)
+                {
+                    isWandering = true;
+                    StartCoroutine(Wander());
+                }
+                break;
+            case AIState.NAVIGATING:
+                if (!isNavigating)
+                {
+                    isNavigating = true;
+                    StartCoroutine(NavigateToPlayer());
+                }
+                break;
+            case AIState.DESTROYING:
+                if (!isDestroying)
+                {
+                    isDestroying = true;
+                    StartCoroutine(DestroyStructure());
+                }
+                break;
+        }
+    }
+
+    private void SetNextWaypoint()
+    {
+        int closestWaypointID = 0;
+        float shortestDistToWaypoint = Mathf.Infinity;
+        bool resetVisitedWaypoints = true;
+        for (int i = 0; i < wanderWaypoints.Length; ++i)
+        {
+            float distToWaypoint = Vector3.Distance(wanderWaypoints[i].transform.position, transform.position);
+            if (distToWaypoint < shortestDistToWaypoint && visitedWaypoints.All(w => w != i))
+            {
+                shortestDistToWaypoint = distToWaypoint;
+                closestWaypointID = i;
+                resetVisitedWaypoints = false;
+            }
+        }
+        target = wanderWaypoints[closestWaypointID].transform;
+        if (resetVisitedWaypoints)
+        {
+            visitedWaypoints = new int[wanderWaypoints.Length];
+        }
+        visitedWaypoints[closestWaypointID] = currentWaypoint = closestWaypointID;
+    }
+
+    private IEnumerator Wander()
+    {
+        while (isWandering)
+        {
+            if(Vector3.Distance(target.position, transform.position) <= waypointMinReachDist)
+            {
+                SetNextWaypoint();
+            }
+            yield return wanderRefreshDuration;
+        }
+    }
+
+    private IEnumerator NavigateToPlayer()
+    {
+        yield return navigateRefreshDuration;
+    }
+
+    private IEnumerator DestroyStructure()
+    {
+        yield return destroyRefreshDuration;
+    }
+}
