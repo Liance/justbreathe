73e1ca38bce81f1ec7593d36e2464927405e7d39
Jonathan Li <convair@Honor_10_Lite-14ffa035991.localdomain>
2020-02-01 17:28:40 +0000

    Added camera fix.

---
+++ Assets/3rdPerson+Fly/Scripts/PlayerScripts/CameraSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject activatableCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activatableCamera.GetComponent<Camera>().enabled = false;
+        activatableCamera.GetComponent<Camera>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
