ff9e53009a0d8b32b26e002a359666abe723956b
Kristiyan <stoyanov.krs@gmail.com>
2020-02-02 15:51:33 +0000

    Implemented sound on spawn & collision for sheep

---
+++ Assets/PlaySheepAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySheepAudio : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip[] clips;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = clips[Random.Range(0, clips.Length - 1)];
+    }
+    private void Start()
+    {
+        source.PlayOneShot(clips[Random.Range(0, clips.Length - 1)]);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        source.PlayOneShot(clips[Random.Range(0, clips.Length - 1)]);
+    }
+}
