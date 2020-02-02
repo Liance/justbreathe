e79fa9802ec06f22addac757fe6b33531bceb34e
Kristiyan <stoyanov.krs@gmail.com>
2020-02-02 12:03:54 +0000

    Merged changes

---
+++ Assets/3rdPerson+Fly/Scripts/PlayerScripts/PlayerUITracker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUITracker : MonoBehaviour
+{
+    public Image healthBar;
+    private PlayerStatsManager statsScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statsScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatsManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = statsScript.currentOxygen / statsScript.maxOxygen;
+    }
+}
